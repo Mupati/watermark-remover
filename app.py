8669e08d3db2f34cbfa2c4358ed43c06deb292ab
Mupati <scientificgh@gmail.com>
2021-02-17 16:27:04 +0000

    Add download button

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, render_template, jsonify
 
 from PIL import Image, ImageEnhance
+from flask.helpers import send_from_directory, url_for
 from pdf2image import convert_from_path
 import tempfile
+import os
+from werkzeug.utils import secure_filename
 
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# 20MB file size limit
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
 
 # Image Processing Functions
-
 def pdf_to_images(pdf_path: str) -> list:
     with tempfile.TemporaryDirectory() as path:
         pil_images = convert_from_path(pdf_path, output_folder=path)
@@ -25,7 +33,6 @@ def increase_contrast(pil_image):
 def remove_watermark_with_pil(pil_images: list) -> None:
     res = []
     for img in pil_images:
-        # im = Image.open(filename)
         im = increase_contrast(img)
         R, G, B = im.convert('RGB').split()
         r = R.load()
@@ -45,23 +52,63 @@ def remove_watermark_with_pil(pil_images: list) -> None:
     return res
 
 
-def convert_images_to_pdf(pil_images: list):
-    pil_images[0].save('cleaned_score.pdf', save_all=True,
+def convert_images_to_pdf(pil_images: list, filename):
+    processed_file_path = os.path.join(
+        app.config['UPLOAD_FOLDER'], 'processed_'+filename+'.pdf')
+    pil_images[0].save(processed_file_path, save_all=True,
                        append_images=pil_images[1:])
+    return 'processed_'+filename+'.pdf'
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Routes and Endpoints
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/upload/<path:filename>')
+def processed_file_dir(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 @app.route('/process', methods=['POST'])
 def proccess_file():
-    image_file = request.files['file']
-    image_file.save("./"+image_file.filename)
-    return jsonify(status="Ok", message="Conversion complete")
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify(status="error", message="Missing image or PDF file"), 422
+
+    uploadedFile = request.files['file']
+
+    if uploadedFile.filename == '':
+        return jsonify(status="error", message="No selected file"), 422
+
+    if uploadedFile and allowed_file(uploadedFile.filename):
+        filename = secure_filename(uploadedFile.filename)
+        uploadedFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        stored_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        pil_images = pdf_to_images(pdf_path=stored_file)
+        images_no_watermarks = remove_watermark_with_pil(pil_images=pil_images)
+
+        name_without_extension = filename.split('.')[0]
+        processed_file = convert_images_to_pdf(
+            images_no_watermarks, name_without_extension)
+
+        # After Processing, delete the uploaded file
+        if os.path.exists(stored_file):
+            os.remove(stored_file)
+
+        # Return the path to the processed file to be downloaded
+        return jsonify(status="success", message="Conversion complete",
+                       processed_file_path=url_for('processed_file_dir', filename=processed_file)), 200
+    else:
+        return jsonify(status="error", message="Supported files are: pdf, jpg, png or jpeg"), 422
 
 
 if __name__ == "__main__":
